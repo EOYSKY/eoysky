b6053cac46c706e265930dfa6168fc337bb079de
Jonny Chang <jonny6015@icloud.com>
2021-01-28 21:55:52 +0800

    初始化项目结构

---
+++ app/test/src/test/java/com/eoysky/base/AbstractBaseTest.java
@@ -0,0 +1,14 @@
+/*
+  EOYSKY.COM Inc. Copyright (c) 2014-2021 All Rights Reserved.
+ */
+package com.eoysky.base;
+
+/**
+ * AbstractBaseTest
+ *
+ * @author jonny
+ * @version AbstractBaseTest.java, v 0.1 2021年01月28日 9:17 下午 jonny
+ */
+public class AbstractBaseTest {
+
+}
---
+++ app/ucenter/src/main/java/com/eoysky/ucenter/UcenterApplication.java
@@ -0,0 +1,13 @@
+package com.eoysky.ucenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UcenterApplication.class, args);
+    }
+
+}
